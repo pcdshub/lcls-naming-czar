647d9096f87063d90cd11a20f4eb49a57e095954
Alex Wallace <awallace@slac.stanford.edu>
2018-12-19 16:48:57 -0800

    Flake8

--- tests/test_czar.py
+++ tests/test_czar.py
@@ -1,24 +1,29 @@
 import czar
 import pytest
 expectedKeys = ['functionalTaxon', 'functionalClass', 'functionalIncrement',
-'beamPath', 'fungibleTaxon', 'component', 'componentIncrement', 'elementTaxon']
+                'beamPath', 'fungibleTaxon', 'component', 'componentIncrement',
+                'elementTaxon']
 
 testName = 'EM1K2:GEM:VGC:02:OPN_REQ'
 badName = 'XLEM1K2G:GEM:VGC:02:OPN_REQ'
 
-#Add a name to this list to make sure it stays valid for all time
+
+# Add a name to this list to make sure it stays valid for all time
 @pytest.mark.parametrize("Name", [
     'EM1K2:GEM:VGC:02:OPN_REQ'
 ])
 def test_describe_valid_name(Name):
     assert czar.describe(Name), Name + " is invalid."
 
+
 def test_describe_invalid_name():
     with pytest.raises(ValueError):
         czar.describe(badName)
 
+
 def test_one_works_with_the_other():
     assert czar.name(czar.describe(testName)) == testName
+
 
 @pytest.mark.parametrize("key,thing", [
     ("functionalTaxon", "EM1K2"),
